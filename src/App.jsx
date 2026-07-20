96f152919bdaef6c0c948a76d3d4a41e701e46fb
ShabihaS <shabiha.sultana@gmx.com>
2026-07-20 19:41:01 +1000

    Add book details modal and reusable button component

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import { searchBooks } from "./services/bookApi";
 import BookGrid from "./components/BookGrid/BookGrid";
 import Loader from "./components/Loader/Loader";
 import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+
+  // added for modal
+  const [selectedBook, setSelectedBook] = useState(null);
 
   const handleSearch = async (query) => {
     try {
@@ -46,9 +50,22 @@ function App() {
 
         {error && <ErrorMessage message={error} />}
 
-        {!loading && !error && <BookGrid books={books} />}
+        {!loading && !error && (
+          <BookGrid 
+            books={books}
+            onSelect={setSelectedBook}
+          />
+        )}
 
       </main>
+
+      {selectedBook && (
+        <Modal
+          book={selectedBook}
+          onClose={() => setSelectedBook(null)}
+        />
+      )}
+
     </>
   );
 }
--- src/components/BookGrid/BookGrid.jsx
+++ src/components/BookGrid/BookGrid.jsx
@@ -1,13 +1,14 @@
 import BookCard from "../BookCard/BookCard";
 import classes from "./BookGrid.module.scss";
 
-function BookGrid({ books }) {
+function BookGrid({ books, onSelect }) {
   return (
     <section className={classes.bookGrid}>
       {books.map((book) => (
         <BookCard
           key={book.id}
           book={book}
+          onSelect={onSelect}
         />
       ))}
     </section>
